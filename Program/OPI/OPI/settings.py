9898c82a6631cf1f934b4b9f96857a65513dd773
Anita Kosobucka <anita@kosobucka.pl>
2021-10-10 10:16:06 +0200

    zmiana ustawiec bazy

--- Program/OPI/OPI/settings.py
+++ Program/OPI/OPI/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'OPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Optifolio_db',
+        'USER' : 'postgres',
+        'PASSWORD' : 'qY0jzfIC',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
     }
 }
 
